79cfd87e11a874e51cfc1007b77f1bb5ed980c7d
sandip_behera <sandipbehera321@gmail.com>
2025-06-18 01:31:12 +0530

    prediction pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformationConfig
 from src.components.data_transformation import DataTransformation
 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -59,3 +62,5 @@ if __name__=="__main__":
     print("Test Array:", test_arr)
     print("Preprocessor Path:", preprocessor_path)
     
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
